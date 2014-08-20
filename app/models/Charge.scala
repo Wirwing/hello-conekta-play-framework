6a24f77ec6f09be0dd5f10e6da263ed44c99e7ef
Wirwing <irving.cf@gmail.com>
2014-08-20 15:09:19 -0500

    Fixin amount and card id bugs.

--- app/models/Charge.scala
+++ app/models/Charge.scala
@@ -11,8 +11,14 @@ object Charge{
 
   def generateCharge(user: User, product: Product) : Charge = {
 
-    val chargeData = Map("amount" -> product.price.toInt, "description" -> product.description, 
-                          "card" -> user.conektaUserId, "currency" -> "mxn")
+    //Use customer id as card id, to make charge with customer's default card.
+    val cardId = user.conektaUserId.get
+
+    //Add two trailing zeros to match conekta amount format
+    val conektaAmount = product.price.toInt * 100
+
+    val chargeData = Map("amount" -> conektaAmount, "description" -> product.description, 
+                          "card" -> cardId, "currency" -> "mxn")
     val remoteCharge = com.conekta.Charge.create(chargeData)
 
     if (remoteCharge.status != "paid"){
